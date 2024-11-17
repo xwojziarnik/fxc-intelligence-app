bca97e00b798754e682e0040695a5ac90ede2aff
Wojciech Ziarnik <woj.ziarnik@gmail.com>
2024-11-17 11:45:01 +0100

    feat: entrypoint file containing commands for each microservice

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+case "$1" in
+  receiver)
+    echo "Loading env variables..."
+    source .env
+    echo "Starting microservice receiver..."
+    echo "Starting cron..."
+    cron -f &
+    echo "Starting python..."
+    python -u receiver.py
+    ;;
+  producer)
+    echo "Loading env variables..."
+    source .env
+    echo "Starting producer..."
+    python -u main.py
+    ;;
+  *)
+    echo "Error: Unknown command '$1'."
+    exit 1
+    ;;
+esac
