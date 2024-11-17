13758fb29e7523e2c96b27c54d53d08237daecd8
Wojciech Ziarnik <woj.ziarnik@gmail.com>
2024-11-17 10:43:31 +0100

    feat: utilities (connecting to dbs, saving transactions, updating balances, callback)

---
+++ utils.py
@@ -0,0 +1,96 @@
+import time
+from json import loads
+from typing import Any
+
+from pika import BlockingConnection, ConnectionParameters, PlainCredentials
+from pika.adapters.blocking_connection import BlockingChannel
+from pika.exceptions import AMQPConnectionError
+from pika.spec import BasicProperties
+from psycopg2 import OperationalError, connect
+from psycopg2.extensions import connection, cursor
+
+from settings import settings
+
+
+def connect_to_postgres(
+    username: str, password: str, host: str, port: str, db_name: str
+) -> connection:
+    attempt_count = 0
+    while attempt_count < 10:
+        try:
+            db_connection: connection = connect(
+                dbname=db_name, user=username, password=password, host=host, port=port
+            )
+            return db_connection
+        except OperationalError:
+            attempt_count += 1
+            print(f"Reconnecting to PostgreSQL, {attempt_count=}")
+            time.sleep(2)
+    else:
+        print("Couldn't connect to PostgreSQL")
+        return
+
+
+def connect_to_rabbitmq(username: str, password: str, host: str, port: str) -> BlockingChannel:
+    attempt_count = 0
+    while attempt_count < 10:
+        try:
+            credentials = PlainCredentials(username, password)
+            connection = BlockingConnection(
+                ConnectionParameters(host=host, port=port, credentials=credentials)
+            )
+            return connection.channel()
+        except AMQPConnectionError:
+            attempt_count += 1
+            print(f"Reconnecting to RabbitMQ, {attempt_count=}")
+            time.sleep(2)
+    else:
+        print("Couldn't connect to RabbitMQ")
+        return
+
+
+def save_transaction_to_postgres(account_id: int, transaction_amount: int) -> None:
+    db_connection: connection = connect_to_postgres(
+        username=settings.postgres_db_user,
+        password=settings.postgres_db_password,
+        db_name=settings.postgres_db_name,
+        host=settings.postgres_db_host,
+        port=settings.postgres_db_port,
+    )
+    db_cursor: cursor = db_connection.cursor()
+    db_cursor.execute(
+        "INSERT INTO historical_transactions (provider_id, transaction_value) VALUES (%s, %s)",
+        (account_id, transaction_amount),
+    )
+    db_cursor.commit()
+    print(f"Saved transaction: {account_id}, {transaction_amount}")
+    db_connection.close()
+
+
+def update_provider_balance(providers_id: int, amount: int) -> None:
+    db_connection: connection = connect_to_postgres(
+        username=settings.postgres_db_user,
+        password=settings.postgres_db_password,
+        db_name=settings.postgres_db_name,
+        host=settings.postgres_db_host,
+        port=settings.postgres_db_port,
+    )
+    db_cursor: cursor = db_connection.cursor()
+    update_sql_command: str = """ UPDATE initial_data
+            SET initial_value = initial_value + %s
+            WHERE id = %s"""
+    db_cursor.execute(update_sql_command, (amount, providers_id))
+    db_connection.commit()
+    print(f"Updated balance for id: {providers_id}, with value: {amount}")
+    db_connection.close()
+
+
+def callback_after_receiving_a_message(
+    ch: BlockingChannel, method: Any, properties: BasicProperties, body: bytes
+):
+    data = loads(body.decode("utf-8"))
+    print(f" [x] Received {data}")
+    account_id = data["id"]
+    transaction_amount = data["value"]
+    save_transaction_to_postgres(account_id=account_id, transaction_amount=transaction_amount)
+    update_provider_balance(providers_id=account_id, amount=transaction_amount)
